fe16c6bbfda48eb9a123808b8a85c92a7c2a9ae0
Lucy <lucy70381@kimo.com>
2023-03-04 00:53:39 +0800

    feat: add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    '.src//pages/**/*.{js,ts,jsx,tsx}',
+    './src/layout/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '12px',
+        lg: '56px',
+      },
+    },
+    extend: {
+      backgroundImage: {
+        Taipei: './images/Taipei.png',
+        logo: 'url("/images/logo.svg")',
+      },
+      boxShadow: {
+        '3xl': '0 4px 3px 0 rgba(13, 11, 12, 0.2)',
+      },
+      content: {
+        triangle: 'url("/icons/triangle.svg")',
+        triangle2: 'url("/icons/triangle2.svg")',
+        square: 'url("/icons/square.svg")',
+        square2: 'url("/icons/square2.svg")',
+        circle: 'url("/icons/circle.svg")',
+      },
+      colors: {
+        primary: '#FF1D6C',
+        secondary: '#FFB72C',
+        green: '#007350',
+        background: '#F6F7FB',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/line-clamp')],
+};
