36d50616ca223bf17736d20b9fd4d0da6bb54f8d
Lucy <lucy70381@kimo.com>
2023-03-04 01:20:42 +0800

    feat: add network images domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    if (!config.experiments) {
+      config.experiments = {};
+    }
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
+  images: {
+    domains: ['www.trimt-nsa.gov.tw', 'travel.nantou.gov.tw', 'taiwan.taiwanstay.net.tw', 'www.sunmoonlake.gov.tw', 'www.taiwan.net.tw'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
